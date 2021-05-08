79c6bfeea616bfa7409078110e65a2ff9f3403bb
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-05-08 19:55:55 +0300

    Consolidate imports and text types

--- tools/tool-base/src/main/java/io/spine/tools/dart/fs/DartFile.java
+++ tools/tool-base/src/main/java/io/spine/tools/dart/fs/DartFile.java
@@ -24,13 +24,46 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package io.spine.tools.dart.fs;
+
+import com.google.common.collect.ImmutableList;
+import io.spine.tools.fs.ExternalModule;
+import io.spine.tools.fs.FileWithImports;
+
+import java.nio.file.Path;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * This package contains types for wording with Dart code.
+ * A Dart source file.
  */
-@CheckReturnValue
-@ParametersAreNonnullByDefault
-package io.spine.tools.dart.code;
+public final class DartFile extends FileWithImports {
 
-import com.google.errorprone.annotations.CheckReturnValue;
+    private DartFile(Path path) {
+        super(path);
+    }
 
-import javax.annotation.ParametersAreNonnullByDefault;
+    /**
+     * Reads the file from the local file system.
+     */
+    public static DartFile read(Path path) {
+        checkNotNull(path);
+        DartFile file = new DartFile(path);
+        file.load();
+        return file;
+    }
+
+    @Override
+    protected boolean isImport(String line) {
+        return ImportStatement.declaredIn(line);
+    }
+
+    @Override
+    protected String resolveImport(String line,
+                                   Path libPath,
+                                   ImmutableList<ExternalModule> modules) {
+        ImportStatement statement = new ImportStatement(this, line);
+        ImportStatement resolved = statement.resolve(libPath, modules);
+        return resolved.toString();
+    }
+}
