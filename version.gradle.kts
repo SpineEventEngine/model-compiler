b9ee76451a559a53070727442342eb1d7fa2f4b7
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-11 20:02:27 +0200

    Bump versions

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,5 +25,5 @@
  */
 
 val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.74")
-val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.74")
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.77")
+val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.75")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.78")
